59d68f3d6458ff8da8d7f446fc557fde79db6dc3
Pedro Dutra <pedro@flizzet.com>
2019-03-03 02:18:33 -0800

    Changed hover effects on level changer to animations triggered on hover & click

--- assets/js/Positioning.js
+++ assets/js/Positioning.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     positionLevelChanger();
+    levelChangerHovering();
+    
 });
 
 function positionLevelChanger() {
@@ -8,4 +10,14 @@ function positionLevelChanger() {
     let levelText = $('#current-level-text');
     let lftVal = parseInt(getComputedStyle(document.body).getPropertyValue('--fixed-margin'));
     changerButtons.css('right', lftVal + levelText.height() + (lftVal / 2));
+}
+
+function levelChangerHovering() {
+    $('#level-changer .level-button').on('click mouseenter', function() {
+        $(this).addClass("triggered");
+
+        setTimeout(function() {
+            $(this).removeClass("triggered");
+        }.bind(this), 500);
+    });
 }
