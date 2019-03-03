f54deff7ad36f847e8d5ef9951415b47d326a4c4
Pedro Dutra <pedro@flizzet.com>
2019-03-03 01:36:05 -0800

    Added anim.css, fixed sticky hover effects for level changer hover

--- assets/js/Positioning.js
+++ assets/js/Positioning.js
@@ -5,9 +5,7 @@ $(document).ready(function() {
 function positionLevelChanger() {
     let changerButtons = $('#level-changer');
 
-    // Get x of level text
     let levelText = $('#current-level-text');
     let lftVal = parseInt(getComputedStyle(document.body).getPropertyValue('--fixed-margin'));
-    console.log(levelText.height());
     changerButtons.css('right', lftVal + levelText.height() + (lftVal / 2));
 }
