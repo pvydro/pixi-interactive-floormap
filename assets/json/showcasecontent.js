b4375fada4eb591ca0cedce1a9d5dba48158a90f
Pedro Dutra <pedro@flizzet.com>
2019-03-05 23:44:18 -0800

    Made showcase page responsive

--- assets/json/showcasecontent.js
+++ assets/json/showcasecontent.js
@@ -3,12 +3,14 @@ var ShowcaseContentJSON = {
     {
         id: "Theater",
         name: "Theater",
-        description: "Theater data description"
+        description: "Theater data description",
+        image: "livingroom_01.jpg"
     },
     GuestBath:
     {
         id: "Guest Bathroom",
         name: "Guest Bathroom",
-        description: "The guest bathrooms are accompanied with two, fully equiped bathrooms including a shower, two sinks, and everything one would need to freshen up."
+        description: "The guest bathrooms are accompanied with two, fully equiped bathrooms including a shower, two sinks, and everything one would need to freshen up.",
+        image: "livingroom_01.jpg"
     }
 }
