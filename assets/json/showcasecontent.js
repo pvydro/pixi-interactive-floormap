aa5198cf5b762c6091f67c314b24fe86932a4f82
Pedro Dutra <pedro@flizzet.com>
2019-03-16 08:03:06 -0700

    Removed unused css link from showcasepage.html, changed text-align of header to right, updated showcasecontent with new informative paragraphs

--- assets/json/showcasecontent.js
+++ assets/json/showcasecontent.js
@@ -26,7 +26,7 @@ var ShowcaseContentJSON = {
     Kitchen: {
         id: "Kitchen",
         name: "Kitchen",
-        description: "The kitchen features a very nice island with pots hanging from the ceiling, along with enough seating space for 12 people on an extremely well crafted concrete table.",
+        description: "The kitchen features a very nice island table with pots hanging from the ceiling, along with enough seating space for 12 people on an extremely well crafted concrete table.",
         image: "kitchen.jpg"
     },
     SituationRoom: {
@@ -50,31 +50,31 @@ var ShowcaseContentJSON = {
     GearRoom: {
         id: "Gear Room",
         name: "Gear Room",
-        description: "",
+        description: "There's not much to say about this room besides the insipiration behind the layout, which is a firemans locker area. This is a great space for storing personal items you do not want near your bed.",
         image: "gearroom.jpg"
     },
     SkyBridge: {
         id: "Skybridge",
         name: "Skybridge",
-        description: "",
+        description: "This skybridge serves as a passage between the building section with the pool and the bunk room to the rest of the house. It also has an amazing view.",
         image: "skybridge.jpg"
     },
     Library: {
         id: "Library",
         name: "Library",
-        description: "",
+        description: "This is one of the most beautiful rooms of the house. Featuring an extremely extensive showcase of books, a massive TV that has every streaming service you will ever need and an XBOX One on top of all that. With this room, boredom can never be the answer. ",
         image: "library.jpg"
     },
     Entryway: {
         id: "Entryway",
         name: "Entryway",
-        description: "",
+        description: "The entryway to the home is a beautiful wheelchair friendly walkway that has (insert tree type) trees spanning across the sides",
         image: "entryway.jpg"
     },
     Deck: {
         id: "Deck",
         name: "Deck",
-        description: "",
+        description: "The lake deck has speakers built into the roofing that allow you to listen to music not just indoors but outdoors aswell. The lake deck is placed right above the solar panels and has an otherwordly view of the mountains summer fall winter and spring.",
         image: "deck.jpg"
     }
 }
